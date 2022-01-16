bf5a507598e568445e9d482e079c7fc804d8190c
MaxSouza88 <souza1988@hotmail.com>
2022-01-16 18:02:41 -0300

    Alterando JS

--- JSDESAFIO.js
+++ JSDESAFIO.js
@@ -1,9 +1,22 @@
 const imagem = document.querySelector('img');
+const imagemSegundo = document.querySelector('img');
+const imagemTerceiro = document.querySelector('img');
+
 const botao = document.querySelector('button');
-const botaoSegundo = document.querySelector('button');
+const botaoS = document.querySelector('button');
+const botaoT = document.querySelector('button');
+
 const nomeDoPersonagem = document.querySelector('#primeiroNome');
+const nomeDoSegundoPersonagem = document.querySelector('#segundoNome');
+const nomeDoTerceiroPersonagem = document.querySelector('#terceiroNome');
+
 const especieDoPersonagem = document.querySelector('#primeiroEspecie');
+const especieDoSegundoPersonagem = document.querySelector('#segundoEspecie');
+const especieDoTerceiroPersonagem = document.querySelector('#terceiroEspecie');
+
 const statusDoPersonagem = document.querySelector('#primeiroCondicao');
+const statusDoSegundoPersonagem = document.querySelector('#segundoCondicao');
+const statusDoTerceiroPersonagem = document.querySelector('#terceiroCondicao');
 
 gerarValorAleatorio = () =>{
     return Math.floor(Math.random() * 671);
@@ -18,17 +31,64 @@ return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
         Accept: 'application/json',
         "Content-type": 'application/json'
     }
+
 }).then((response) => response.json()).then((data) => {
     imagem.src = data.image;
     imagem.alt = data.name;
+    
     nomeDoPersonagem.innerHTML = data.name;
-    especieDoPersonagem.innerHTML = data.species;''
+    especieDoPersonagem.innerHTML = data.species;
     statusDoPersonagem.innerHTML = data.status;
 });
 }
-/*botao = pegarPersonagem
-botaoSegundo = pegarSegundoPersonagem
 
-onclick = (botao); (botaoSegundo);*/
+gerarValorAleatorioS = () =>{
+    return Math.floor(Math.random() * 671);
+}
 
-botao.onclick = pegarPersonagem;
+pegarPersonagemS = () => {
+let numeroAleatorioS = gerarValorAleatorioS();
+
+return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorioS}`, {
+    method:'GET',
+    headers: {
+        Accept: 'application/json',
+        "Content-type": 'application/json'
+    }
+}).then((response) => response.json()).then((data) => {
+    imagemSegundo.src = data.image;
+    imagemSegundo.alt = data.name;
+
+    nomeDoSegundoPersonagem.innerHTML = data.name;
+    especieDoSegundoPersonagem.innerHTML = data.species;
+    statusDoSegundoPersonagem.innerHTML = data.status;
+});
+}
+
+gerarValorAleatorioT = () =>{
+    return Math.floor(Math.random() * 671);
+}
+
+pegarPersonagemT = () => {
+let numeroAleatorioT = gerarValorAleatorioT();
+
+return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorioT}`, {
+    method:'GET',
+    headers: {
+        Accept: 'application/json',
+        "Content-type": 'application/json'
+    }
+}).then((response) => response.json()).then((data) => {
+    imagemTerceiro.src = data.image;
+    imagemTerceiro.alt = data.name;
+
+    nomeDoTerceiroPersonagem.innerHTML = data.name;
+    especieDoTerceiroPersonagem.innerHTML = data.species;
+    statusDoTerceiroPersonagem.innerHTML = data.status;
+});
+}
+
+
+botaoS.onclick = pegarPersonagemS;
+
+
